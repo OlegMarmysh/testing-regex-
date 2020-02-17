04332d9ed6843563d47d3b2c10ee1087ebeb696b
Bbara <backdown959@gmail.com>
2020-02-17 17:49:40 +0300

    first commit

---
+++ test.js
@@ -0,0 +1,18 @@
+var startValueText = document.getElementById('start-value');
+var btnDecrypt = document.getElementById('btn-decrypt');
+var answerValueText = document.getElementById('answer');
+
+
+btnDecrypt.addEventListener('click', edwardDecrypt);
+
+
+function edwardDecrypt(message) {
+    var valueFormat = /[а-яА-ЯA-Z]{1,}/g;
+    message = startValueText.value;
+    if(valueFormat.test(message)){
+        alert('a message can only consist of lowercase English letters');
+    }else{
+        answerValueText.innerHTML = (message.replace(/([a-z])\1/g, '$1'));
+    }
+    
+}
